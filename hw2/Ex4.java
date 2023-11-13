2af9a4145e7a786981adf91ac50e16aa51fe0369
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2023-11-13 10:36:44 +0300

    exercise 4

---
+++ hw2/Ex4.java
@@ -0,0 +1,16 @@
+public class Ex4 {
+    public static void main(String[] args) {
+
+int s=5678;
+int a=s%10;
+s=s/10;
+int b=s%10;
+s=s/10;
+int c=s%10;
+s=s/10;
+System.out.print(a);
+System.out.print(b);
+System.out.print(c);
+System.out.print(s);
+   }
+   }
