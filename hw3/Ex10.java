48191c2f164320a37acc0297fb5623520f4d567c
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2023-11-14 08:56:58 +0300

    exercise 10

---
+++ hw3/Ex10.java
@@ -0,0 +1,12 @@
+public class Ex10 {
+    public static void main(String[] args) {
+        int result = 1;
+        int a=10;
+        int b=15;
+        for (int i = a; i <= b; i++) {
+           result = result * i;
+        }
+        System.out.println(result);
+      }
+
+}
