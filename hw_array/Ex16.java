b68f4a36be2e357a14af99ba6885816ad6637217
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2023-11-14 15:05:26 +0300

    exercise 16 not ready !!!

---
+++ hw_array/Ex16.java
@@ -0,0 +1,26 @@
+
+public class Ex16 {
+    public static void main(String[] args) {
+        int array [] = {1,7,5,6,3,15,10,5,4,6,4,2,8,6,9};
+        
+        System.out.print(array.length)
+        
+        int cap=0;
+        int index=0;
+        for (int i = 0; i < array.length; i++) {
+             if (array[i]> cap){
+                cap=array[i];
+                index=i;
+             }   
+            }
+            System.out.printf("Max значение - %s В ячейке %s", cap, index);
+            
+            for (int i = array.length-1; i >= 0; i--) {
+                if (array[i]< cap){
+                   cap=array[i];
+                   index=i;
+                }   
+               }
+            System.out.printf("Min значение - %s В ячейке %s", cap, index);       
+    }
+    }
