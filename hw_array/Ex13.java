5c9ba72b5d4f716d211b5a27adc0cd561a2ddea9
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2023-11-14 13:32:09 +0300

    exercise 13

---
+++ hw_array/Ex13.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class Ex13 {
+    public static void main(String[] args) {
+        Random rand = new Random(); 
+        int upperbound = 101;
+
+        int array[] = new int[10];
+        for(int i=0; i < 10; i++){
+            array [i] = rand.nextInt(upperbound);
+            }
+
+        int n=0;
+        for (int i = 0; i < 10; i++) {
+             if (array[i]> n){
+                n=array[i];
+             }   
+            }
+            System.out.println ("Максимальное число = "+n);        
+    }
+    }
