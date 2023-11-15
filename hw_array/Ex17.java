9cd0462447d81c0aca74ab51713818ec27c9ee7f
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2023-11-15 10:51:42 +0300

    exercise 17

---
+++ hw_array/Ex17.java
@@ -0,0 +1,35 @@
+/*Создать двухмерный квадратный массив, и заполнить его «бабочкой», т.е
+таким образом:
+1 1 1 1 1
+0 1 1 1 0
+0 0 1 0 0
+0 1 1 1 0
+1 1 1 1 1*/
+public class Ex17 {
+    public static void main(String[] args) {
+        int arr[][]=new int[10][10];
+        int i,j;
+
+        for (i=0;i<arr.length/2+1;i++){
+            for (j=0;j<arr[i].length;j++){
+                if ((j<i) || (j>=(arr[i].length-i)))
+                    arr[i][j]=0;
+                else
+                    arr[i][j]=1;
+            }
+        }
+        for (i=arr.length-1; i>=arr.length/2+1; i--){
+            for (j=0; j<arr[i].length; j++){
+                if ((j<(arr[i].length-1-i)) || (j>i))
+                    arr[i][j]=0;
+                else
+                    arr[i][j]=1;}
+        }
+        for (i=0;i<arr.length;i++){
+            for(j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+}
